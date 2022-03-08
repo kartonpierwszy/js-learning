52bb82a149dc49e5139b93d1fab3fd23fca3836e
Karton Pierwszy <kartonpierwszy@gmail.com>
2022-03-08 11:44:38 +0100

    Learning and practice

---
+++ js/basic_operators.js
@@ -0,0 +1,75 @@
+//BASIC OPERATORS
+///////////////////////////
+//
+/*
+//Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 *2 = 8
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+//
+//Assigment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+console.log(x);
+//
+// COMPARISON OPERATORS
+console.log(`Jonas is ${ageJonas}, Sarah is ${ageSarah}`);
+console.log(ageJonas > ageSarah); // >, <, >=, <=,
+console.log(ageSarah >= 18);
+
+const isFuulAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+// OPERATOR PROCEDENCE
+// documentation https://developer.mozilla.org/pl/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(`Jonas is ${ageJonas}, Sarah is ${ageSarah}`);
+
+console.log(now - 1991 > now - 2018);
+//
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+//
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+*/
+/*
+//
+/////////////////////////
+// CHALLENGE #1
+/////////////////////////
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 95;
+const heightJohn = 1.76;
+//;
+const bmiJonas = massJohn / heightJohn ** 2;
+const bmiMark = massMark / heightMark ** 2;
+console.log(`Jonas BMI is ${bmiJonas}, where Mark BMI is ${bmiMark}`);
+//
+const markHigherBMIa = bmiMark > bmiJonas;
+console.log(markHigherBMIa);
+//
+const markHigherBMIb = massMark / heightMark ** 2 > massJohn / heightJohn ** 2;
+console.log(markHigherBMIb);
+*/
