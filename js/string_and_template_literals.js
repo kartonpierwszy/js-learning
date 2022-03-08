9890f17db6e3037a5dc50bc8cede4e7103eff63b
Karton Pierwszy <kartonpierwszy@gmail.com>
2022-03-08 11:49:47 +0100

    Learning and practice

---
+++ js/string_and_template_literals.js
@@ -0,0 +1,23 @@
+// STRING AND TEMPLATE LITERALS
+/////////////////////////
+//
+/*
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+//
+// V1
+const jonasV1 =
+  "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+console.log(jonasV1);
+// V2
+const jonasV2 = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(jonasV2);
+//multiple   line strings
+console.log('String with \n multiple \n lines.');
+console.log(`String with
+multiple
+ lines.`);
+ */
+//
