cadfc54c727b19810a4b26e29ddba032bc580cdf
Karton Pierwszy <kartonpierwszy@gmail.com>
2022-03-20 13:58:07 +0100

    Practice

--- js/practise.js
+++ js/practise.js
@@ -1,8 +1,1 @@
-language = 'portuguese';
-const country = 'Portugal';
-const continent = 'Europe';
-const isIsland = false;
-// isIsland = true;
-const population = 20;
-
-console.log(population / 2);
+ 
