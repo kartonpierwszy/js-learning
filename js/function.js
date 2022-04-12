9273cd10ccccebb50db55d48445ba390508596d6
Karton Pierwszy <kartonpierwszy@gmail.com>
2022-04-12 12:53:22 +0200

    Function declarations... with function word

--- js/function.js
+++ js/function.js
@@ -1,2 +1,27 @@
 //FUNCTIONS
 //
+
+function logger() {
+  console.log('My name is Ralph');
+}
+// callin / running / invoking function
+logger();
+logger();
+logger();
+//
+function fruitProcessor(apples, oranges) {
+  // console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+//
+// POWER of the function!
+// reuse the function with different input values
+// and then get a differrent output!
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+const num = '23';
